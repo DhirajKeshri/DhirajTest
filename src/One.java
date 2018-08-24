078ad95b10f07975a935422bbba61752e0d73cb6
DhirajKeshri <Dhirajk0612@gmail.com>
2018-08-24 20:05:53 +0530

    Update One.java

--- src/One.java
+++ src/One.java
@@ -1,11 +1,37 @@
 
 public class One {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello world");
-System.out.println("hi");	
-System.out.println("Hello world");
+	
+	String FilePath = Helper.Filepath("Container") ;                  //Excel file call in constructor
+	FileInputStream	fs = new FileInputStream(FilePath);  // file Path 
+	XSSFWorkbook work = new XSSFWorkbook(fs);
+	XSSFSheet containerSheet = work.getSheet("Container");
+	
+	String ContainerNumber = null;
+	for(int i = 0;i<containerSheet.getLastRowNum();i++)
+	{
+		try {
+			if((containerSheet.getRow(i).getCell(1).getStringCellValue()).equalsIgnoreCase(""))
+			{
+				ContainerNumber =  containerSheet.getRow(i).getCell(0).getStringCellValue().trim();
+				//Row row = S.createRow(i);
+				
+			}
+					
+		}
+		catch (Exception e) 
+		{
+			Row row = containerSheet.getRow(i);
+			row.createCell(1).setCellValue("Used");
+			FileOutputStream fos = new FileOutputStream(FilePath);
+            work.write(fos);
+            fos.close();
+           break;
+		}
+	}	
+	work.close();
+	return ContainerNumber;
+
 }
 
 }
